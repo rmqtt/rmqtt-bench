69769dc721d87731f41bf77823c35ffa0ad132ea
rmqtt <rmqttd@126.com>
2022-10-18 20:56:13 +0800

    Initial commit

---
+++ src/main-v3.rs
@@ -0,0 +1,217 @@
+use std::sync::Arc;
+use std::time::Duration;
+
+use futures::StreamExt;
+use ntex::rt::spawn;
+use ntex::time::sleep;
+use ntex::util::ByteString;
+use ntex_mqtt::v3::codec::Publish;
+use ntex_mqtt::v3::QoS;
+use once_cell::sync::OnceCell;
+use parking_lot::RwLock;
+use rust_box::event::Event;
+use rust_box::std_ext::StdExt;
+use structopt::StructOpt;
+
+use options::Options;
+use stats::Stats;
+use v3::Client;
+
+type DashMap<K, V> = dashmap::DashMap<K, V, ahash::RandomState>;
+
+mod options;
+mod script;
+mod stats;
+mod v3;
+
+#[ntex::main]
+async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("RUST_LOG", "rmqtt_bench=info");
+    env_logger::init();
+
+    let opts = Options::from_args();
+    log::info!("{:?}", opts);
+
+    v3_benchmarks(opts).await;
+
+    Ok(())
+}
+
+async fn v3_benchmarks(opts: Options) {
+    let mgr = ControlManager::init(Arc::new(opts.clone()));
+    for i in 0..opts.conns {
+        let c = v3::Client::new(i, mgr.clone());
+        mgr.add_client(c);
+    }
+
+    let mut exit_futs = Vec::new();
+    for i in 0..mgr.clients_len() {
+        if let Some(c) = mgr.client(i) {
+            let exit_fut = c.start().await;
+            exit_futs.push(exit_fut.into_future());
+        }
+    }
+
+    std::thread::spawn(|| loop {
+        std::thread::sleep(Duration::from_secs(5));
+        log::info!("{}", Stats::instance().to_string());
+    });
+
+    futures::future::join_all(exit_futs).await;
+    log::info!("{}", Stats::instance().to_string());
+}
+
+pub type TopicFilter = ByteString;
+pub type Topic = ByteString;
+pub type Retain = bool;
+pub type Payload = ntex::util::Bytes;
+pub type PacketId = u16;
+
+#[derive(Clone)]
+pub struct ControlManager {
+    opts: Arc<Options>,
+    clients: Arc<RwLock<Vec<Client>>>,
+    connecteds: Arc<DashMap<String, Client>>,
+    disconnecteds: Arc<DashMap<String, Client>>,
+
+    on_connect: Arc<Event<Client, bool>>,
+    on_connected: Arc<Event<Client, ()>>,
+    on_disconnected: Arc<Event<Client, ()>>,
+    on_subscribe: Arc<Event<(Client, TopicFilter, QoS), bool>>,
+    on_subscribed: Arc<Event<(Client, TopicFilter, QoS), ()>>,
+    on_publish: Arc<Event<(Client, PacketId, Topic, QoS, Retain, Payload), bool>>,
+    on_publish_ack: Arc<Event<(Client, PacketId), ()>>,
+    on_message: Arc<Event<(Client, Publish), ()>>,
+}
+
+unsafe impl Send for ControlManager {}
+
+unsafe impl Sync for ControlManager {}
+
+static CONTROL_MANAGER: OnceCell<ControlManager> = OnceCell::new();
+
+impl ControlManager {
+    pub fn init(opts: Arc<Options>) -> &'static ControlManager {
+        CONTROL_MANAGER.set(Self::new(opts)).ok().unwrap();
+        CONTROL_MANAGER.get().unwrap()
+    }
+
+    pub fn instance() -> &'static ControlManager {
+        CONTROL_MANAGER.get().unwrap()
+    }
+
+    fn new(opts: Arc<Options>) -> Self {
+        Self {
+            opts,
+            clients: Arc::new(RwLock::new(Vec::new())),
+            connecteds: Arc::new(DashMap::default()),
+            disconnecteds: Arc::new(DashMap::default()),
+
+            on_connect: Event::listen(|_args, _next| true).finish().arc(),
+            on_connected: Event::listen(|c: Client, _next| {
+                ControlManager::instance().add_connected(c);
+                ()
+            })
+                .finish()
+                .arc(),
+            on_disconnected: Event::listen(|c: Client, _next| {
+                ControlManager::instance().add_disconnected(c);
+                ()
+            })
+                .finish()
+                .arc(),
+            on_subscribe: Event::listen(|_args, _next| true).finish().arc(),
+            on_subscribed: Event::listen(|_args, _next| ()).finish().arc(),
+            on_publish: Event::listen(|_args, _next| true).finish().arc(),
+            on_publish_ack: Event::listen(|_args, _next| ()).finish().arc(),
+            on_message: Event::listen(|_args, _next| ()).finish().arc(),
+        }
+            .control()
+    }
+
+    fn add_client(&self, c: Client) {
+        self.clients.write().push(c);
+    }
+
+    fn clients_len(&self) -> usize {
+        self.clients.read().len()
+    }
+
+    fn client(&self, idx: usize) -> Option<Client> {
+        self.clients.read().get(idx).cloned()
+    }
+
+    fn connecteds_len(&self) -> usize {
+        self.connecteds.len()
+    }
+
+    fn add_connected(&self, c: Client) {
+        self.disconnecteds.remove(&c.client_id);
+        self.connecteds.insert(c.client_id.clone(), c);
+    }
+
+    fn add_disconnected(&self, c: Client) {
+        self.connecteds.remove(&c.client_id);
+        self.disconnecteds.insert(c.client_id.clone(), c);
+    }
+
+    fn control(self) -> Self {
+        //The control center
+        let mgr = self.clone();
+        let runner = async move {
+            let ctrl_interval = Duration::from_millis(mgr.opts.ctrl_interval);
+            let disconn_ratio = mgr.opts.ctrl_disconn_ratio;
+            let count_8 = (mgr.opts.conns as f32 * (1.0 - disconn_ratio)) as usize;
+            let count_2 = (mgr.opts.conns as f32 * disconn_ratio) as usize;
+            let mgr1 = mgr.clone();
+            let disconnects = |n: usize| {
+                let mut count = 0;
+                let mut cids = Vec::new();
+                for c in mgr1.connecteds.iter() {
+                    if c.disconnect() {
+                        cids.push(c.client_id.clone());
+                        count += 1;
+                    }
+                    if count >= n {
+                        break;
+                    }
+                }
+                for cid in &cids {
+                    mgr1.connecteds.remove(cid);
+                }
+            };
+
+            let mgr2 = mgr.clone();
+            let connects = |n: usize| {
+                let mut cids = Vec::new();
+                for (i, c) in mgr2.disconnecteds.iter().enumerate() {
+                    if i >= n {
+                        break;
+                    }
+                    c.connect();
+                    cids.push(c.client_id.clone());
+                }
+                for cid in &cids {
+                    mgr2.disconnecteds.remove(cid);
+                }
+            };
+
+            loop {
+                // log::info!("total: {:?}, connecteds: {}, disconnecteds: {}", mgr.clients_len(), mgr.connecteds_len(), mgr.disconnecteds_len());
+                if mgr.connecteds_len() >= count_8 {
+                    disconnects(count_2);
+                }
+
+                if mgr.connecteds_len() < mgr.opts.conns {
+                    connects(count_2);
+                }
+
+                sleep(ctrl_interval).await;
+            }
+        };
+        if self.opts.control {
+            spawn(runner);
+        }
+        self
+    }
+}
---
+++ src/v3.rs
@@ -0,0 +1,443 @@
+use std::convert::TryFrom;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::time::Duration;
+
+use futures::channel::mpsc;
+use futures::SinkExt;
+use futures::StreamExt;
+use ntex::time;
+use ntex::time::{Seconds, sleep};
+use ntex::util::Bytes;
+use ntex::util::ByteString;
+use ntex::util::Ready;
+use ntex_mqtt::{self, v3};
+use ntex_mqtt::error::SendPacketError;
+use ntex_mqtt::v3::codec::SubscribeReturnCode;
+use parking_lot::{Mutex, RwLock};
+use tokio::task::spawn_local;
+use uuid::Uuid;
+
+use super::{ControlManager, PacketId};
+use super::Options;
+use super::Stats;
+
+pub enum Message {
+    Connect,
+    Disconnect,
+    Close,
+}
+
+#[derive(Clone)]
+pub struct Client {
+    pub client_id: String,
+    pub seq_no: String,
+    // pub opts: Arc<Options>,
+    pub sink: Arc<RwLock<Option<v3::MqttSink>>>,
+    pub mgr: ControlManager,
+    subs: Arc<AtomicUsize>,
+    msg_tx: Arc<RwLock<Option<mpsc::Sender<Message>>>>,
+    disconnected: Arc<AtomicBool>,
+    closed: Arc<AtomicBool>,
+    packet_id_gen: Arc<Mutex<u16>>,
+}
+
+impl Client {
+    pub fn new(seq_no: usize, mgr: ControlManager) -> Self {
+        let seq_no = format!("{}", seq_no);
+
+        //{no}-{random}
+        let client_id = mgr
+            .opts
+            .client_id_pattern
+            .clone()
+            .replace("{random}", Uuid::new_v4().to_string().as_str())
+            .replace("{no}", seq_no.as_str());
+
+        Self {
+            client_id,
+            seq_no,
+            sink: Arc::new(RwLock::new(None)),
+            mgr,
+            subs: Arc::new(AtomicUsize::new(0)),
+            msg_tx: Arc::new(RwLock::new(None)),
+            disconnected: Arc::new(AtomicBool::new(false)),
+            closed: Arc::new(AtomicBool::new(false)),
+            packet_id_gen: Arc::new(Mutex::new(1)),
+        }
+    }
+
+    pub fn gen_packet_id(&self) -> PacketId {
+        let mut id = self.packet_id_gen.lock();
+        let packet_id = *id;
+        if packet_id < u16::MAX {
+            *id += 1;
+            packet_id
+        } else {
+            *id = 2;
+            1
+        }
+    }
+
+    pub fn is_closed(&self) -> bool {
+        self.closed.load(Ordering::SeqCst)
+    }
+
+    pub fn close(&self) -> bool {
+        let mut ret = false;
+        if let Some(sink) = self.sink() {
+            if sink.is_open() {
+                Stats::instance().closeds.inc();
+                sink.close();
+                self.closed.store(true, Ordering::SeqCst);
+                ret = true;
+                if let Some(mut msg_tx) = self.msg_tx() {
+                    let _ = msg_tx.try_send(Message::Close);
+                }
+            }
+        }
+        ret
+    }
+
+    pub fn is_disconnected(&self) -> bool {
+        self.disconnected.load(Ordering::SeqCst)
+    }
+
+    pub fn disconnect(&self) -> bool {
+        let mut ret = false;
+        if let Some(sink) = self.sink() {
+            if sink.is_open() {
+                sink.close();
+                self.disconnected.store(true, Ordering::SeqCst);
+                ret = true;
+                if let Some(mut msg_tx) = self.msg_tx() {
+                    let _ = msg_tx.try_send(Message::Disconnect);
+                }
+            }
+        }
+        ret
+    }
+
+    pub fn connect(&self) {
+        self.disconnected.store(false, Ordering::SeqCst);
+        if let Some(mut msg_tx) = self.msg_tx() {
+            let _ = msg_tx.try_send(Message::Connect);
+        }
+    }
+
+    fn set_sink(&self, sink: v3::MqttSink) {
+        self.sink.write().replace(sink);
+    }
+
+    pub fn sink(&self) -> Option<v3::MqttSink> {
+        self.sink.read().as_ref().cloned()
+    }
+
+    fn msg_tx(&self) -> Option<mpsc::Sender<Message>> {
+        self.msg_tx.read().as_ref().cloned()
+    }
+
+    fn subs_inc(&self) {
+        Stats::instance().subs.inc();
+        self.subs.fetch_add(1, Ordering::SeqCst);
+    }
+
+    fn subs_clear(&self) {
+        Stats::instance().subs.decs(self.subs_count() as isize);
+        self.subs.store(0, Ordering::SeqCst);
+    }
+
+    pub fn subs_count(&self) -> usize {
+        self.subs.load(Ordering::SeqCst)
+    }
+
+    pub async fn start(&self) -> mpsc::Receiver<()> {
+        let client = self.clone();
+
+        let client_id = client.client_id.clone();
+        let opts = client.mgr.opts.clone();
+        let no = client.seq_no.clone();
+        let mgr = client.mgr.clone();
+
+        let (mut exit_tx, exit_rx) = futures::channel::mpsc::channel(10);
+        let (mut msg_tx, mut msg_rx) = mpsc::channel::<Message>(100);
+
+        self.msg_tx.write().replace(msg_tx.clone());
+        let _ = msg_tx.send(Message::Connect).await;
+
+        let addr = opts.addr();
+        let mut builder = v3::client::MqttConnector::new(addr.clone())
+            .client_id(client_id.clone())
+            .keep_alive(Seconds(opts.keepalive))
+            .handshake_timeout(Seconds(opts.handshake_timeout));
+
+        if let (Some(username), Some(password)) = (opts.username.as_ref(), opts.password.as_ref()) {
+            builder = builder
+                .username(username.clone())
+                .password(Bytes::from(password.clone()));
+        }
+
+        if opts.clear_session {
+            builder = builder.clean_session()
+        };
+
+        if let Some(last_will) = opts.last_will.to_last_will() {
+            builder = builder.last_will(last_will);
+        }
+
+        let qos = v3::codec::QoS::try_from(opts.qos).unwrap();
+        let runner = async move {
+            let sleep_interval: Duration = Duration::from_millis(opts.auto_reconn_interval);
+
+            use tokio::time::timeout;
+
+            while !client.is_closed() {
+                match timeout(sleep_interval, msg_rx.next()).await {
+                    Err(_) => {
+                        log::debug!("timeout ...");
+                        if client.is_disconnected() {
+                            continue;
+                        }
+                    }
+                    Ok(None) => break,
+                    Ok(Some(Message::Connect)) => {
+                        log::debug!("Message::Connect ...");
+                    }
+                    Ok(Some(Message::Disconnect)) => {
+                        log::debug!("Message::Disconnect ...");
+                        continue;
+                    }
+                    Ok(Some(Message::Close)) => {
+                        log::debug!("Message::Close ...");
+                        break;
+                    }
+                }
+
+                client.subs_clear();
+
+                let connect_enable = mgr.on_connect.fire(client.clone());
+
+                if connect_enable {
+                    match builder.connect().await {
+                        Ok(c) => {
+                            mgr.on_connected.fire(client.clone());
+
+                            let sink = c.sink();
+
+                            client.set_sink(sink.clone());
+
+                            Stats::instance().conns.inc();
+
+                            //subscribe
+                            if opts.sub_switch {
+                                let topic = ByteString::from(
+                                    opts.topic_pattern
+                                        .clone()
+                                        .replace("{cid}", client_id.as_str())
+                                        .replace("{no}", no.as_str()),
+                                );
+
+                                let subscribe_enable =
+                                    mgr.on_subscribe.fire((client.clone(), topic.clone(), qos));
+
+                                if subscribe_enable {
+                                    spawn_local(Self::subscribe(
+                                        client.clone(),
+                                        sink.clone(),
+                                        topic.clone(),
+                                        qos,
+                                        client_id.clone(),
+                                    ));
+                                }
+                            }
+
+                            //publish
+                            if opts.pub_switch && opts.pub_payload_len > 0 {
+                                let opts = opts.clone();
+                                let client_id = client_id.clone();
+                                let mut exit_tx = exit_tx.clone();
+                                let client = client.clone();
+                                spawn_local(async move {
+                                    Self::publishs(client.clone(), sink.clone(), opts, client_id)
+                                        .await;
+                                    client.close();
+                                    let _ = exit_tx.send(()).await;
+                                });
+                            }
+
+                            //client event loop
+                            Self::ev_loop(c, client.clone()).await;
+                        }
+                        Err(e) => {
+                            log::debug!("{:?} connect to {:?} fail, {:?}", client_id, addr, e);
+                            Stats::instance().conn_fails.inc();
+                        }
+                    }
+                }
+            }
+            let _ = exit_tx.send(()).await;
+        };
+        spawn_local(runner);
+
+        exit_rx
+    }
+
+    async fn subscribe(
+        c: Client,
+        sink: v3::MqttSink,
+        sub_topic: ByteString,
+        qos: v3::QoS,
+        client_id: String,
+    ) {
+        'subscribe: loop {
+            match sink
+                .subscribe()
+                .topic_filter(sub_topic.clone(), qos)
+                .send()
+                .await
+            {
+                Ok(rets) => {
+                    for ret in rets {
+                        if let SubscribeReturnCode::Failure = ret {
+                            log::debug!("{:?} subscribe failure", client_id);
+                            time::sleep(Duration::from_secs(5)).await;
+                            continue 'subscribe;
+                        } else {
+                            c.subs_inc();
+                        }
+                    }
+                    c.mgr
+                        .on_subscribed
+                        .fire((c.clone(), sub_topic.clone(), qos));
+                    break;
+                }
+                Err(SendPacketError::Disconnected) => {
+                    log::debug!("{:?} subscribe error, Disconnected", client_id);
+                    break;
+                }
+                Err(e) => {
+                    log::debug!("{:?} subscribe error, {:?}", client_id, e);
+                    break;
+                }
+            }
+        }
+    }
+
+    async fn publishs(c: Client, sink: v3::MqttSink, opts: Arc<Options>, client_id: String) {
+        let (no_start, no_end) = if let Some(range) = &opts.pub_topic_no_range {
+            match range.len() {
+                0 => (0, opts.conns as u64),
+                1 => (0, range[0]),
+                _ => (range[0], range[1]),
+            }
+        } else {
+            (0, opts.conns as u64)
+        };
+
+        let pub_topic_len = no_end - no_start;
+
+        let qos = v3::codec::QoS::try_from(opts.qos).unwrap();
+
+        loop {
+            sleep(Duration::from_millis(opts.pub_interval)).await;
+
+            let no = (rand::prelude::random::<u64>() % pub_topic_len) + no_start;
+            let pub_topic = ByteString::from(
+                opts.topic_pattern
+                    .clone()
+                    .replace("{cid}", &client_id)
+                    .replace("{no}", &format!("{}", no)),
+            );
+
+            let payload = ntex::util::Bytes::from("0".repeat(opts.pub_payload_len));
+
+            if sink.is_open() {
+                let packet_id = c.gen_packet_id();
+
+                let publish_enable = c.mgr.on_publish.fire((
+                    c.clone(),
+                    packet_id,
+                    pub_topic.clone(),
+                    qos,
+                    opts.retain,
+                    payload.clone(),
+                ));
+
+                if publish_enable {
+                    let mut pub_builder = sink.publish(pub_topic, payload);
+                    if opts.retain {
+                        pub_builder = pub_builder.retain();
+                    }
+                    //pub_builder.packet_id()
+                    if opts.pub_max_limit > 0 {
+                        if !Stats::instance().sends.inc_limit(opts.pub_max_limit) {
+                            break;
+                        }
+                    } else {
+                        Stats::instance().sends.inc();
+                    }
+                    if opts.qos == 0 {
+                        if let Err(e) = pub_builder.send_at_most_once() {
+                            log::debug!("{:?} publish error, {:?}", client_id, e);
+                        }
+                    } else {
+                        let pub_builder = pub_builder.packet_id(packet_id);
+                        if let Err(e) = pub_builder.send_at_least_once().await {
+                            log::debug!("{:?} publish error, {:?}", client_id, e);
+                        } else {
+                            c.mgr.on_publish_ack.fire((c.clone(), packet_id));
+                        }
+                    }
+                }
+            } else if Stats::instance().sends.value() >= opts.pub_max_limit {
+                break;
+            } else {
+                sleep(Duration::from_secs(5)).await;
+            }
+        }
+    }
+
+    async fn ev_loop(c: v3::Client, client: Client) {
+        if let Err(e) = c
+            .start(
+                move |control: v3::client::ControlMessage<()>| match control {
+                    v3::client::ControlMessage::Publish(publish) => {
+                        Stats::instance().recvs.inc();
+                        client
+                            .mgr
+                            .on_message
+                            .fire((client.clone(), publish.packet().clone()));
+                        Ready::Ok(publish.ack())
+                    }
+                    #[allow(deprecated)]
+                    v3::client::ControlMessage::Disconnect(msg) => {
+                        log::debug!("Server disconnecting: {:?}", msg);
+                        Ready::Ok(msg.ack())
+                    }
+                    v3::client::ControlMessage::Error(msg) => {
+                        log::debug!("Codec error: {:?}", msg);
+                        Ready::Ok(msg.ack())
+                    }
+                    v3::client::ControlMessage::ProtocolError(msg) => {
+                        log::debug!("Protocol error: {:?}", msg);
+                        Ready::Ok(msg.ack())
+                    }
+                    v3::client::ControlMessage::PeerGone(msg) => {
+                        log::debug!("Peer closed connection: {:?}", msg.err());
+                        Ready::Ok(msg.ack())
+                    }
+                    v3::client::ControlMessage::Closed(msg) => {
+                        log::debug!("Server closed connection: {:?}", msg);
+                        client.subs_clear();
+                        Stats::instance().conns.dec();
+                        client.mgr.on_disconnected.fire(client.clone());
+                        Ready::Ok(msg.ack())
+                    }
+                },
+            )
+            .await
+        {
+            log::error!("start ev_loop error! {:?}", e);
+        }
+    }
+}
